d3d2023176a50f1fc40a98a32c4170cf4c7a28cd
SupKCH <92071402+SupKCH@users.noreply.github.com>
2021-10-10 19:46:32 +0700

    Update calc_adaptive_ASHRAE.py

--- calc_adaptive_ASHRAE.py
+++ calc_adaptive_ASHRAE.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  7 13:17:52 2021
-
-@author: SC
-"""
 from pythermalcomfort.models import adaptive_ashrae
 from pythermalcomfort.utilities import running_mean_outdoor_temperature
-from pprint import pprint
+#from pprint import pprint
+from flask import Flask, request
 
-result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=23, v=0.3)
-
-pprint(result)
-
-print(result["acceptability_80"])
-
-result = adaptive_ashrae(tdb=77, tr=77, t_running_mean=73.5, v=1, units="IP")
-
-pprint(result)
-
-print(result["acceptability_80"])
-
-rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
-
-result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
-pprint(result)
+app = Flask(__name__)
+#books = []
+@app.route('/', methods=['POST', 'GET'])
+def cal():
+  if request.method == 'POST':
+    #body = request.get_json()
+    #books.append(body)
+    
+    rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
+    result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
+    #pprint(result)
+    
+    return { "message": "Book already add to database", "body": result }, 201
+  elif request.method == 'GET':
+    rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
+    result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
+    return { "books": result }, 200
+  
