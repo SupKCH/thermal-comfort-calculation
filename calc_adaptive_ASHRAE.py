0a16d408fad1af43e437cb85c4ef7d804ef039b9
SupKCH <92071402+SupKCH@users.noreply.github.com>
2021-10-11 12:49:51 +0700

    Update calc_adaptive_ASHRAE.py

--- calc_adaptive_ASHRAE.py
+++ calc_adaptive_ASHRAE.py
@@ -1,25 +1,11 @@
 from pythermalcomfort.models import adaptive_ashrae
 from pythermalcomfort.utilities import running_mean_outdoor_temperature
-#from pprint import pprint
 from flask import Flask, request
 
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def cal():
   if request.method == 'POST':
-    
-    #rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
-    #result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
-    #pprint(result)
-    
-    #if not request.json or not 'title' in request.json:
-    #    abort(400)
-    #task = {
-    #    'title': request.json['title'],
-    #    'description': request.json.get('description', ""),
-    #    'done': False
-    #}
-    
     if not request.json or not 'title' in request.json or not 'tdb' in request.json or not 'tr' in request.json or not 'tod_list' in request.json or not 'v' in request.json:
         abort(400)
     task = {
@@ -35,7 +21,5 @@ def cal():
     return {'task': task, 'result': result}, 201
   
   elif request.method == 'GET':
-    #rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
-    #result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
     message = 'Hey!, this is GET request. I want POST request'
     return  message, 200
