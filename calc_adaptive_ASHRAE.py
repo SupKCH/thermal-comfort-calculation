c0fb57114d8f95fc3cbf386036c83e5e1965feaf
SupKCH <92071402+SupKCH@users.noreply.github.com>
2021-10-11 11:49:04 +0700

    Update calc_adaptive_ASHRAE.py

--- calc_adaptive_ASHRAE.py
+++ calc_adaptive_ASHRAE.py
@@ -11,23 +11,24 @@ def cal():
     #rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
     #result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
     #pprint(result)
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
     
-    #if not request.json or not 'title' in request.json or not 'tdb' in request.json or not 'tr' in request.json:
+    #if not request.json or not 'title' in request.json:
     #    abort(400)
     #task = {
     #    'title': request.json['title'],
-    #    'tdb': request.json['tdb'],
-    #    'tr': request.json['tr']
+    #    'description': request.json.get('description', ""),
+    #    'done': False
     #}
     
-    return {'task': task}, 201
+    if not request.json or not 'title' in request.json or not 'tdb' in request.json or not 'tr' in request.json:
+        abort(400)
+    task = {
+        'title': request.json['title'],
+        'tdb': request.json['tdb'],
+        'tr': request.json['tr']
+    }
+    
+    return task, 201
   elif request.method == 'GET':
     rmt_value = running_mean_outdoor_temperature([29, 28, 30, 29, 28, 30, 27], alpha=0.9)
     result = adaptive_ashrae(tdb=25, tr=25, t_running_mean=rmt_value, v=0.3)
