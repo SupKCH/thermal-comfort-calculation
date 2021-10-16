7c9d91a5a9add3e034dc41e213e4f95f35f050ff
SupKCH <92071402+SupKCH@users.noreply.github.com>
2021-10-16 14:02:57 +0700

    First time setup

--- calc_pmv_ppd_heroku.py
+++ calc_pmv_ppd_heroku.py
@@ -2,23 +2,34 @@ from pythermalcomfort.models import pmv_ppd
 from pythermalcomfort.utilities import v_relative, clo_dynamic
 from pythermalcomfort.utilities import met_typical_tasks
 from pythermalcomfort.utilities import clo_individual_garments
+from flask import Flask, request
 
-tdb = 27
+#tdb = 27
 tr = 25
-v = 0.1  # average air speed, [m/s]
-rh = 50
+v = 0.1
+#rh = 50
 activity = "Typing"
 garments = ["Sweatpants", "T-shirt", "Shoes or sandals"]
-
 met = met_typical_tasks[activity]
 icl = sum(
     [clo_individual_garments[item] for item in garments]
 )
-
 vr = v_relative(v=v, met=met)
 clo = clo_dynamic(clo=icl, met=met)
 
-results = pmv_ppd(tdb=tdb, tr=tr, vr=vr, rh=rh, met=met, clo=clo, standard="ASHRAE")
-
-print(results)
-print(f"pmv={results['pmv']}, ppd={results['ppd']}%")
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def cal():
+  if request.method == 'POST':
+    if not request.json or not 'tdb' in request.json or not 'rh' in request.json:
+        abort(400)
+    task = {
+      'tdb': request.json['tdb'],
+      'rh': request.json['rh'],
+    }    
+    results = pmv_ppd(tdb=task['tdb'], tr=tr, vr=vr, rh=task['rh'], met=met, clo=clo, standard="ASHRAE")
+    return result, 201
+  
+  elif request.method == 'GET':
+    message = 'Hey!, this is GET request. I want POST request'
+    return  message, 200
